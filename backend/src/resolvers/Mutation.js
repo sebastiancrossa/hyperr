0ee42d509f5afd8e7e931ad462ef8f6a890cb1fb
Sebastian Crossa <crossasebastian@gmail.com>
2020-03-05 07:15:36 -0600

    Implement the conection between our local graphql server and our prisma server

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if the user is logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: args.data
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // Use forwarding when the query is exactly the same as the prisma query (no custom logic, authentication, etc)
+  items: forwardTo("db")
+};
 
 module.exports = Query;
