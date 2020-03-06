1b82de450d1b22267200e3ffa186c33ede14fd59
Sebastian Crossa <crossasebastian@gmail.com>
2020-03-05 21:46:37 -0600

    Add resolvers for updating an item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,6 +10,21 @@ const Mutations = {
     );
 
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    // Taking a copy of the updated info from the args
+    const updatedArgs = { ...args };
+
+    // Removing the id from the updated args
+    delete updatedArgs.id;
+
+    return await ctx.db.mutation.updateItem({
+      data: updatedArgs,
+      where: {
+        id: args.id // Getting the id directly from the args, not our object itself
+      },
+      info
+    });
   }
 };
 
