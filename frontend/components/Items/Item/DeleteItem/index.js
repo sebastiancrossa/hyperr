76fbd7c4368da7ac028aa32d026c7d1c23c71123
Sebastian Crossa <crossasebastian@gmail.com>
2020-03-06 20:10:34 -0600

    Add delete item feature (needs to be fixed - doesnt update cache and re render components until page refresh)

--- frontend/components/Items/Item/DeleteItem/index.js
+++ frontend/components/Items/Item/DeleteItem/index.js
@@ -1,6 +1,7 @@
 // Libraries
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "../../index";
 
 // --- GRAPHQL --- //
 const DELETE_ITEM_MUTATION = gql`
@@ -12,18 +13,39 @@ const DELETE_ITEM_MUTATION = gql`
 `;
 // --- --- //
 
-const DeleteItem = ({ toDelete, children }) => {
-  const [deleteItem, { data, loading }] = useMutation(DELETE_ITEM_MUTATION);
+const DeleteItem = ({ toDelete, client, children }) => {
+  const [deleteItem] = useMutation(DELETE_ITEM_MUTATION, {
+    variables: {
+      id: toDelete
+    },
+    update(cache, { data }) {
+      // Manually update our cache on the client side so the website doesnt have to be reloaded
+      // for the effect to be seen
+
+      // Using our exported query to get the item we want
+      // This returns all of the contents of  the cache (including the item we are wanting to delete)
+      const cachedData = cache.readQuery({ query: ALL_ITEMS_QUERY });
+
+      // Filtering the item the users deletes
+      cachedData.items = cachedData.items.filter(
+        item => item.id !== data.deleteItem.id
+      );
+
+      // Putting the items back to the cache
+      cache.writeQuery({
+        query: ALL_ITEMS_QUERY,
+        data: {
+          ...cachedData
+        }
+      });
+    }
+  });
 
   return (
     <button
-      onClick={async () => {
+      onClick={() => {
         if (confirm("Are you sure you want to delete this item?")) {
-          const res = await deleteItem({
-            variables: {
-              id: toDelete
-            }
-          });
+          deleteItem();
         }
       }}
     >
